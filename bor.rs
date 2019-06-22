6993283380fb858c241439fbc7970060875a4df5
Nulik <nuliknol@gmail.com>
2019-06-22 17:42:28 -0500

    init

---
+++ bor.rs
@@ -0,0 +1,22 @@
+use std::fmt;
+
+fn main() {
+
+	#[derive(Debug,Copy,Clone)]
+	struct Xval {
+		x:	u32
+	};
+	impl fmt::Display for Xval {
+    	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        	write!(f, "({})", self.x)
+	    }
+	}
+	let x_val1 = Xval {
+		x:5u32
+	};
+
+	let y_val = x_val1;
+
+	let z_val= x_val1;
+	println!("val={}",z_val);
+}
